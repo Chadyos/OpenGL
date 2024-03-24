d941675fe8e8cd372473d94ebb6c43795f8803ac
Chadyos <foraetan@gmail.com>
2024-03-24 23:31:32 +0100

    push init

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,102 +1,121 @@
+// Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
 
-
+// Include GLEW
 #include <GL/glew.h>
 
+// Include GLFW
 #include <GLFW/glfw3.h>
+GLFWwindow* window;
 
+// Include GLM
 #include <glm/glm.hpp>
 using namespace glm;
 
+#include <common/shader.hpp>
 
-int main(){
-if( !glfwInit() )
+int main( void )
 {
-	fprintf( stderr, "Failed to initialize GLFW\n" );
-	return -1;
-}
+	// Initialize GLFW
+	if( !glfwInit() )
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make macOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow( 1024, 768, "Tutorial 02 - Red triangle", NULL, NULL);
+	if( window == NULL ){
+		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
 
 
+	static const GLfloat g_vertex_buffer_data[] = { 
+		-0.0f, -1.0f, 0.0f,
+		 -1.0f, -1.0f, 0.0f,
+		 1.0f,  1.0f, 0.0f,
+	};
 
-
-glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
-glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // On veut OpenGL 3.3
-glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Pour rendre MacOS heureux ; ne devrait pas être nécessaire
-glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // On ne veut pas l'ancien OpenGL
-
-// Ouvre une fenêtre et crée son contexte OpenGl
-GLFWwindow* window; // (Dans le code source qui accompagne, cette variable est globale)
-window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL);
-if( window == NULL ){
-	fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-	glfwTerminate();
-	return -1;
-}
-glfwMakeContextCurrent(window); // Initialise GLEW
-glewExperimental=true; // Nécessaire dans le profil de base
-if (glewInit() != GLEW_OK) {
-	fprintf(stderr, "Failed to initialize GLEW\n");
-	return -1;
-}
-
-
-// Assure que l'on peut capturer la touche d'échappement enfoncée ci-dessous
-glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-
-
-GLuint VertexArrayID;
-glGenVertexArrays(1, &VertexArrayID);
-glBindVertexArray(VertexArrayID);
-
-static const GLfloat g_vertex_buffer_data[] = {
-   -1.0f, -1.0f, 0.0f,
-   1.0f, -1.0f, 0.0f,
-   0.0f,  1.0f, 0.0f,
-};
-
-
-// This will identify our vertex buffer
-GLuint vertexbuffer;
-// Generate 1 buffer, put the resulting identifier in vertexbuffer
-glGenBuffers(1, &vertexbuffer);
-// The following commands will talk about our 'vertexbuffer' buffer
-glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-// Give our vertices to OpenGL.
-glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-
-
-
-do{
-	// Clear the screen to black
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	// 1rst attribute buffer : vertices
-	glEnableVertexAttribArray(0);
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glVertexAttribPointer(
-	   0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-	   3,                  // size
-	   GL_FLOAT,           // type
-	   GL_FALSE,           // normalized?
-	   0,                  // stride
-	   (void*)0            // array buffer offset
-	);
-	// Draw the triangle !
-	glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-	glDisableVertexAttribArray(0);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	glfwSwapBuffers(window);
-	glfwPollEvents();
+	do{
 
-} // Vérifie si on a appuyé sur la touche échap (ESC) ou si la fenêtre a été fermée
-while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-glfwWindowShouldClose(window) == 0 );
+		// Clear the screen
+		glClear( GL_COLOR_BUFFER_BIT );
 
+		// Use our shader
+		glUseProgram(programID);
 
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
 
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
 
+		glDisableVertexAttribArray(0);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
+	// Cleanup VBO
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteVertexArrays(1, &VertexArrayID);
+	glDeleteProgram(programID);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
 }
+
